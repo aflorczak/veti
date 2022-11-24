7cd7e6aa483cbc46ea430e0b6cb88235a1a14ff8
Adrian Florczak <af_site@outlook.com>
2022-11-24 11:31:10 +0100

    done - Set NODE_ENV and PORT in .env

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 import app from './app.js';
 
-const server = app.listen(80, () => {
-    console.log(`Running`);
+app.set('NODE_ENV', process.env.NODE_ENV || 'PRODUCTION');
+app.set('PORT', process.env.PORT || 80);
+
+const server = app.listen(app.get('PORT'), () => {
+    console.log(`
+    Running:
+    ========================
+    NODE_ENV: ${app.get('NODE_ENV')}
+    PORT: ${app.get('PORT')}
+    ========================
+    `);
 });

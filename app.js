1e066cf6ffbb40d20d8ee3c4991d3da7a9e1d9b7
Adrian Florczak <af_site@outlook.com>
2022-11-24 11:22:46 +0100

    done - Create first Handlebars Page.

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
+import { create } from 'express-handlebars';
 
 const app = express();
+const hbs = create({
+    helpers: {
+        getDomain() { return process.env.DOMAIN },
+        getSlogan() { return process.env.SLOGAN}
+    }
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 app.get('/', (req,res) => {
-    res.send("Hello Veti");
+    res.render('pages/home/index', {
+        msg: 'Hello Veti',
+        title: "Hello Veti"
+    });
 });
 
 export default app;
